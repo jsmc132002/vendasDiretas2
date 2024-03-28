540ec9dfbdd992be099b1824c9a5733738807bb4
jsmc13 <jsmc132002@yahoo.com.br>
2024-03-28 12:05:18 -0300

    redefinindo repositorio

--- vendasDiretas/appVendasDiretas/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from appVendasDiretas.models import Fornecedor, Produto, Cliente, Pedido
-
-# Register your models here.
-admin.site.register(Fornecedor)
-admin.site.register(Produto)
-admin.site.register(Cliente)
-admin.site.register(Pedido)
--- vendasDiretas/appVendasDiretas/urls.py
+++
@@ -1,8 +0,0 @@
-from appVendasDiretas import views
-from django.urls import path
-
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path("cadastroFornecedor", views.cadastroFornecedor, name='cadadstroFornecedor'),
-]
